d967e8646870dd52821aae64c0e90a1694b1ab46
Arjun <99074495+Wuhuiuusushsg@users.noreply.github.com>
2025-04-15 17:16:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7472777466:AAHdCsYtZpPnoMv67OL5_Hv2yjvy2Nz8jEg"
-    API_ID = 22581733
-    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
-    ADMIN = '6530997270'.split(',')
+    BOT_TOKEN = "8019668327:AAFfvVgnHJq1nC38WgDh604Wk7B3Mr7x8TE"
+    API_ID = 22080412
+    API_HASH = "17a5b12bb9261b0250d31bbef9c5227f"
+    ADMIN = '7587316405'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
     DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DB_NAME = "Crack_Jack"
